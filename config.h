a6ddeb515a27aab015de98a2c4ffb3843f0259a9
chinahehaiyang <imhhy123@hotmail.com>
2019-06-24 19:44:29 -0600

    add movestack in config

--- config.h
+++ config.h
@@ -64,6 +64,7 @@ static const char *termcmd[]  = { "umeWow",  NULL};
 
 static const char *chromecmd[]  = { "chromium",  NULL}; 
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -72,6 +73,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
