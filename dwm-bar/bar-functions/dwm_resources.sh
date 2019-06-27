4a52d633f3d3e4d30998e6cfa7d41f4ae655186d
chinahehaiyang <imhhy123@hotmail.com>
2019-06-26 21:30:53 -0600

    changed status bar

--- dwm-bar/bar-functions/dwm_resources.sh
+++ dwm-bar/bar-functions/dwm_resources.sh
@@ -10,31 +10,43 @@ dwm_resources () {
     MEMTOT=$(free -h |awk '(NR == 2) {print $2}')
     # CPU temperature
     #CPU=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d. -f1)
-	CPU1=$(sensors | grep -i "Core 0:" | awk '{print $3}' | cut -d '+' -f 2 ) 
-	CPU1=${CPU1::-4}
-	CPU2=$(sensors | grep -i "Core 1:" | awk '{print $3}') 
-	CPU2=${CPU2::-4}
-	CPU3=$(sensors | grep -i "Core 2:" | awk '{print $3}') 
-	CPU3=${CPU3::-4}
-	CPU4=$(sensors | grep -i "Core 3:" | awk '{print $3}') 
-	CPU4=${CPU4::-4}
-	CPU=$((CPU1+CPU2+CPU3+CPU4))
-	CPU=$((CPU / 4))
-	CPU=${CPU:0:3}°C
 
-	load=$(uptime | grep load | awk '{print $9}')
-	load=${load::-1}
+	#CPU1=$(sensors | grep -i "Core 0:" | awk '{print $3}' | cut -d '+' -f 2 ) 
+	#CPU1=${CPU1::-4}
+	#CPU2=$(sensors | grep -i "Core 1:" | awk '{print $3}') 
+	#CPU2=${CPU2::-4}
+	#CPU3=$(sensors | grep -i "Core 2:" | awk '{print $3}') 
+	#CPU3=${CPU3::-4}
+	#CPU4=$(sensors | grep -i "Core 3:" | awk '{print $3}') 
+	#CPU4=${CPU4::-4}
+	cpuAll=$(sensors | grep "Core" | awk '{print $3}' | tr -d '°C+' | tr '\n' ' ' )
+	CPU1=$(echo $cpuAll | awk '{print $1}')
+	CPU2=$(echo $cpuAll | awk '{print $2}')
+	CPU3=$(echo $cpuAll | awk '{print $3}')
+	CPU4=$(echo $cpuAll | awk '{print $4}')
+
+	#CPU=$((CPU1+CPU2+CPU3+CPU4))
+	#CPU=$((CPU / 4))
+	#CPU=${CPU:0:3}°C
+
+
+	echo $CPU1
+	echo $CPU2
+	echo $CPU3
+	echo $CPU4
+	CPU=$(echo "($CPU1+$CPU2+$CPU3+$CPU4)"/4 |bc)
+
+	#load=$(uptime | grep load | awk '{print $9}')
+	#load=${load::-1}
+
+	percent=$(mpstat | awk '{print $13}' | tr '\n' ' ' | awk '{print (100 - $2 ) "%"}')
 
     # Used and total storage in /home (rounded to 1024B)
     STOUSED=$(df -h | grep '/sda5' | awk '{print $3}')
     STOTOT=$(df -h | grep  '/sda5' | awk '{print $2}')
     STOPER=$(df -h | grep  '/sda5' | awk '{print $5}')
 
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "💻 MEM: %s/%s|CPU: %s|Load: %s|STO: %s/%s: %s|\n" "$MEMUSED" "$MEMTOT" "$CPU" "$load" "$STOUSED" "$STOTOT" "$STOPER"
-    else
-        printf "[STA | MEM %s/%s CPU %s STO %s/%s: %s]\n" "$MEMUSED" "$MEMTOT" "$CPU" "$STOUSED" "$STOTOT" "$STOPER"
-    fi
+        printf "💻 MEM: %s/%s|CPU: %s%%, used: %s|STO: %s/%s: %s|\n" "$MEMUSED" "$MEMTOT" "$CPU" "$percent" "$STOUSED" "$STOTOT" "$STOPER"
 }
 
 dwm_resources
