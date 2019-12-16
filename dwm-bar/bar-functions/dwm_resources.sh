9b34fa2b8736b3cf26b1f985abe8c6d65f846a82
Haiyang He <imhhy123@hotmail.com>
2019-12-16 16:22:29 -0700

    small changes

--- dwm-bar/bar-functions/dwm_resources.sh
+++ dwm-bar/bar-functions/dwm_resources.sh
@@ -9,36 +9,28 @@ dwm_resources () {
     MEMUSED=$(free -h | awk '(NR == 2) {print $3}')
     MEMTOT=$(free -h |awk '(NR == 2) {print $2}')
     # CPU temperature
-    #CPU=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d. -f1)
 
-	#CPU1=$(sensors | grep -i "Core 0:" | awk '{print $3}' | cut -d '+' -f 2 ) 
-	#CPU1=${CPU1::-4}
-	#CPU2=$(sensors | grep -i "Core 1:" | awk '{print $3}') 
-	#CPU2=${CPU2::-4}
-	#CPU3=$(sensors | grep -i "Core 2:" | awk '{print $3}') 
-	#CPU3=${CPU3::-4}
-	#CPU4=$(sensors | grep -i "Core 3:" | awk '{print $3}') 
-	#CPU4=${CPU4::-4}
-	cpuAll=$(sensors | grep "Core" | awk '{print $3}' | tr -d '°C+' | tr '\n' ' ' )
-	CPU1=$(echo $cpuAll | awk '{print $1}')
-	CPU2=$(echo $cpuAll | awk '{print $2}')
-	CPU3=$(echo $cpuAll | awk '{print $3}')
-	CPU4=$(echo $cpuAll | awk '{print $4}')
+	#For dell intel cpu:
+	#cpuAll=$(sensors | grep "Core" | awk '{print $3}' | tr -d '°C+' | tr '\n' ' ' )
+	#CPU1=$(echo $cpuAll | awk '{print $1}')
+	#CPU2=$(echo $cpuAll | awk '{print $2}')
+	#CPU3=$(echo $cpuAll | awk '{print $3}')
+	#CPU4=$(echo $cpuAll | awk '{print $4}')
 
-	CPUTEMP=$(echo "($CPU1+$CPU2+$CPU3+$CPU4)"/4 |bc)
+	#CPUTEMP=$(echo "($CPU1+$CPU2+$CPU3+$CPU4)"/4 |bc)
 
-	#load=$(uptime | grep load | awk '{print $9}')
-	#load=${load::-1}
+	#For thinkpad amd cpu:
+	CPUTEMP=$(sensors | grep "Tdie" | awk '{print $2}' | tr -d '°C+' | tr '\n' ' ' )
 
 	percent=$(mpstat | awk '{print $13}' | tr '\n' ' ' | awk '{print (100 - $2 ) "%"}')
 	#percent=$(mpstat | awk '{print $4}' | tr '\n' ' ' | awk '{print $3 "%"}')
 
     # Used and total storage in /home (rounded to 1024B)
-    STOUSED=$(df -h | grep '/sda5' | awk '{print $3}')
-    STOTOT=$(df -h | grep  '/sda5' | awk '{print $2}')
-    STOPER=$(df -h | grep  '/sda5' | awk '{print $5}')
+    STOUSED=$(df -h | grep '/nvme0n1p5' | awk '{print $3}')
+    STOTOT=$(df -h | grep  '/nvme0n1p5' | awk '{print $2}')
+    STOPER=$(df -h | grep  '/nvme0n1p5' | awk '{print $5}')
 
-        printf "MEM: %s/%s|CPU: %s, temp: %s°C|STO: %s/%s %s|\n" "$MEMUSED" "$MEMTOT" "$percent" "$CPUTEMP"  "$STOUSED" "$STOTOT" "$STOPER"
+        printf "MEM: %s/%s|CPU: %s, %s°C|STO: %s/%s %s|\n" "$MEMUSED" "$MEMTOT" "$percent" "$CPUTEMP"  "$STOUSED" "$STOTOT" "$STOPER"
 }
 
 dwm_resources
--- dwmAutoStart.sh
+++ dwmAutoStart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ~/.bashrc
 sudo ~/.scripts/startup.sh &
+~/dwm-haiyang/dwm-bar/dwm_bar.sh &
+sudo ~/.scripts/keyboard.sh &
 feh --bg-max $wallpaper
 compton &
 ~/.scripts/screenSaverTimeout.sh &
