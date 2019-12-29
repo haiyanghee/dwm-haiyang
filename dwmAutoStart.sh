eadc28c4dfd03e20158985577e85e7a001131e6d
Haiyang He <imhhy123@hotmail.com>
2019-12-28 20:50:26 -0700

    changed dwmAutostart.sh

--- dwmAutoStart.sh
+++ dwmAutoStart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ~/.bashrc
-sudo ~/.scripts/startup.sh &
+sudo ~/.scripts/hdmi-thinkpad.sh &
+sudo ~/.scripts/connectToWifi.sh &
+sudo ~/.scripts/disable-usb-trigger-wakeup.sh &
 ~/dwm-haiyang/dwm-bar/dwm_bar.sh &
 sudo ~/.scripts/keyboard.sh &
 feh --bg-max $wallpaper
