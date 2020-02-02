994e0a12a17c791edfdd4379282fef1fa4f7693f
Haiyang He <imhhy123@hotmail.com>
2020-02-02 16:23:31 -0700

    changed window script and autostart

--- dwm-bar/bar-functions/dwm_pulse.sh
+++ dwm-bar/bar-functions/dwm_pulse.sh
@@ -9,15 +9,23 @@
 dwm_pulse () {
     VOL=$(pamixer --get-volume-human | tr -d '%')
     if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$VOL" = "muted" ] || [ "$VOL" -eq 0 ]; then
-            printf "[🔇]\n"
-        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "[🔈 %s%%]\n" "$VOL"
-        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "[🔉 %s%%]\n" "$VOL"
+        volstat="$(amixer get Master)"
+        vol=$(echo "$volstat" | grep -o "\[[0-9]\+%\]" | head -1 |sed 's/[^0-9]*//g')
+        #echo $vol
+
+        if [[ ! -z "$(echo "$volstat" | grep -o "\[off\]")" ]]; then
+            vol=0
+            icon="🔇"
+        elif [ "$vol" -gt "70" ]; then
+	        icon="🔊"
+        elif [ "$vol" -lt "30" ]; then
+	        icon="🔈"
         else
-            printf "[🔊 %s%%]\n" "$VOL"
+	        icon="🔉"
         fi
+
+        printf "%s %s%%\\n" "$icon" "$vol"
+
     else
         if [ "$VOL" = "muted" ] || [ "$VOL" -eq 0 ]; then
             printf "[MUTE]\n"
--- dwmAutoStart.sh
+++ dwmAutoStart.sh
@@ -2,11 +2,12 @@
 source ~/.bashrc
 sudo ~/.scripts/hdmi-thinkpad.sh &
 sudo ~/.scripts/connectToWifi.sh &
-sudo ~/.scripts/disable-usb-trigger-wakeup.sh &
+#sudo ~/.scripts/disable-usb-trigger-wakeup.sh &
+#sudo ~/.scripts/reloadPsmouse.sh &
 ~/dwm-haiyang/dwm-bar/dwm_bar.sh &
 sudo ~/.scripts/keyboard.sh &
 feh --bg-max $wallpaper
 compton &
-~/.scripts/screenSaverTimeout.sh &
+~/.scripts/normalScreenSaverTimeout.sh &
 ~/.scripts/redshiftopen.sh &
 ~/dwm-haiyang/dwm-bar/testWeather.sh &	#read the weather
