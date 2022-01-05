2076e73e737a9f8ae7e87304b696b2dc69b00259
Haiyang He <imhhy123@hotmail.com>
2022-01-04 19:21:22 -0700

    configured firefox in the rules ...

--- config.h
+++ config.h
@@ -33,6 +33,14 @@ static const char *colors[][3] = {
 /* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
+/*
+How to check the properties of a client?
+Try running:
+xprop | awk '
+	/^WM_CLASS/{sub(/.* =/, "instance:"); sub(/,/, "\nclass:"); print}
+	/^WM_NAME/{sub(/.* =/, "title:"); print}'
+
+*/
 static const Rule rules[] = {
     /* xprop(1):
      *	WM_CLASS(STRING) = instance, class
@@ -40,8 +48,8 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp",      NULL,       NULL,        0,               1,        -1},
-    {"Firefox",   NULL,       NULL,        1 << 8,          0,        -1},
-    {"chromium",  NULL,       NULL,        0,               0,        -1},
+    {"firefox",   NULL,       NULL,        1 << 8,          0,        -1}
+    // {"chromium",  NULL,       NULL,        0,               0,        -1},
 };
 
 /* layout(s) */
