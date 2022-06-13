a303f155ab2b4bb1c9c0d6c1b34f8ac220c99a7a
Haiyang He <imhhy123@hotmail.com>
2022-06-13 15:33:06 -0600

    added comment

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const int showbar = 1;	  /* 0 means no bar */
 static const int topbar = 1;	  /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=11","Noto Color Emoji:pixelsize=11:antialias=true:autohint=true"};
 static const char dmenufont[] = "monospace:size=11";
-static const char col_gray1[] = "#222222";
+static const char col_gray1[] = "#222222"; //asdf
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
